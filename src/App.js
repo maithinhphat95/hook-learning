fc9b770d04eb378bbd24944ee7f5f89b45181c31
maithinhphat95 <maithinhphat.tdh@gmail.com>
2022-12-01 21:38:22 +0700

    useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import "./App.css";
 
-// Hook useEffect:
-
-//
 /**
- Side Effects
-
+ Hook useEffect:
+ 1. Cập nhật State
+ 2. Cập nhật DOM (mutated)
+ 3. re-Render UI
+ 4. Gọi cleanup func nếu deps thay đổi
+ 5. Gọi useEffect callback
  */
 
 /**
-   1. useEffect(callback)
-   - Goi callback mỗi khi re-render 
-   - Gọi callback sau khi component thêm element vào DOM
-   2. useEffect(callback, [])
-   - Chi goi callback 1 lan sau khi component mounted
-   3. useEffect(callback, [deps])
-   - Callback sẽ được gọi lại mỗi khi deps thay đổi
-   - Nó sẽ kiểm tra deps trước và sau khi render có khác nhau k, nếu === thì nó gọi callback
-   */
-
-/**
- * 1. Callback luôn được gọi khi component mounted
- * 2. Clean up function luôn được gọi trước khi component unmounted
- * 3. Clean up function luôn được gọi trước khi callback được gọi
- */
+  Hook useLayoutEffect
+  1. Cập nhật State
+  2. Cập nhật DOM (mutated)
+  3. Gọi cleanup nếu deps thay đổi (sync)
+  4. Gọi useLayoutEffect callback (sync)
+  5. Render lại UI
+  */
 
 const taps = ["posts", "comments", "albums"];
 const lessions = [
@@ -43,149 +36,31 @@ const lessions = [
   },
 ];
 function Content() {
-  const [title, setTitle] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState("posts");
-  const [toTop, setToTop] = useState(false);
-  const [width, setWidth] = useState(window.innerWidth);
-  const [countdown, setCountdown] = useState(100);
-  const [avatar, setAvatar] = useState();
-  const [lessionId, setLessionId] = useState(1);
-
-  useEffect(() => {
-    // 1. Update DOM
-    document.title = title;
-
-    // 2. Call API
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
-      });
-  }, [type]);
-
-  // 3. DOM event listen: chỉ cần add event listener 1 lần thì từ đó trở đi
-  // mỗi khi event dc kich hoạt thì callback dc gọi
-  useEffect(() => {
-    const handleScroll = () => {
-      setToTop(window.scrollY > 200);
-      // khi scrollY > 200 thì setState liên tục, tuy nhiên k render lại. Do state === pre-state
-    };
-    // Event listener pham vi window, nên sẽ không bị unmount, bị rò rỉ bộ nhớ
-    // Khi component mount lần nữa thì nó sẽ tạo thêm 1 event listener nữa
-    // cần phải remove listener trước khi unmounted
-    window.addEventListener("scroll", handleScroll);
-    // 4. Remove event listener
-    // Cleanup Funtion
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  // // Resize Event
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  // Timer
-  // useEffect(() => {
-  //   const timerId = setInterval(() => {
-  //     setCountdown((pre) => pre - 1);
-  //   }, 1000);
-  //   return () => clearInterval(timerId);
-  // }, []);
-
-  // Create new image Object
-  useEffect(() => {
-    // cleanup
-    return () => {
-      avatar && URL.revokeObjectURL(avatar?.preview);
-    };
-  }, [avatar]);
-
-  const handleChangeAvatar = (e) => {
-    const file = e.target.files[0];
-    file.preview = URL.createObjectURL(file);
-    setAvatar(file);
-
-    // Reset file
-    e.target.value = null;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
   };
+  // UI sẽ render xong rồi mới chạy callback, lên 4 xong nháy 1 cái về 0
+  // useEffect(() => {
+  //   if (count > 3) {
+  //     setCount(0);
+  //   }
+  // }, [count]);
 
-  // Listen event fake chat
-  useEffect(() => {
-    const handleComment = ({ detail }) => {
-      console.log(detail);
-    };
-    window.addEventListener(`lession-${lessionId}`, handleComment);
-    return () => {
-      window.removeEventListener(`lession-${lessionId}`, handleComment);
-    };
-  }, [lessionId]);
+  // Callback sẽ chạy trước khi render
+  useLayoutEffect(() => {
+    if (count > 3) {
+      setCount(0);
+    }
+  }, [count]);
 
   return (
     <div>
       <h1>Hello ae</h1>
-      <h1>Width: {width}</h1>
-      {/* <h1>Countdown: {countdown}</h1> */}
-
-      <input type="file" name="" onChange={handleChangeAvatar} />
-      {avatar && <img src={avatar.preview} alt="" width="80%" />}
-
-      {/* {taps.map((tap) => (
-        <button
-          key={tap}
-          style={type === tap ? { color: "#fff", background: "#333" } : {}}
-          onClick={() => setType(tap)}
-        >
-          {tap}
-        </button>
-      ))} */}
-
-      {/* Input Title */}
-      {/* <input
-        type="text"
-        name=""
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      /> */}
-
-      {/* <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title || post.name}</li>
-        ))}
-      </ul> */}
-
-      <ul>
-        {lessions.map((lession) => (
-          <li
-            key={lession.id}
-            style={{
-              color: lessionId === lession.id ? "red " : "#333",
-            }}
-            onClick={() => setLessionId(lession.id)}
-          >
-            {lession.name}
-          </li>
-        ))}
-      </ul>
-      {toTop && (
-        <button
-          style={{
-            position: "fixed",
-            right: "20px",
-            bottom: "20px",
-          }}
-        >
-          To Top
-        </button>
-      )}
+      <h1>{count}</h1>
+      <button type="" onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 }
